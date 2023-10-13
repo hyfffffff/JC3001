87de3a76c96c47c0d5afe0d91e3fd8a5d94124ff
hyfffffff <108557638+hyfffffff@users.noreply.github.com>
2023-10-13 11:17:27 +0800

    Update DL_cnn.py

--- DL_cnn.py
+++ DL_cnn.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 from PIL import Image
 import glob
+
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ class BHI_Dataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         # Recursively find all png images within root_dir
-        self.image_files = glob.glob(root_dir+'//**//*.png', recursive=True)
+        self.image_files = glob.glob(root_dir+'//**//*.png', recursive = True)
         self.transform = transform
 
     def __len__(self):
@@ -21,7 +22,7 @@ class BHI_Dataset(Dataset):
     def __getitem__(self, idx):
         img_name = self.image_files[idx]
         image = Image.open(img_name)
-        # Extract label from the file name (expects '1' for positive IDC)
+         # Extract label from the file name (expects '1' for positive IDC)
         label = 1 if img_name[-5]=='1' else 0
 
         if self.transform:
@@ -40,12 +41,19 @@ dataset = BHI_Dataset(root_dir='D:\CBIS_DDSM\IDC_regular_ps50_idx5', transform=t
 train_size = int(0.8 * len(dataset))
 val_size = int(0.1 * len(dataset))
 test_size = len(dataset) - train_size - val_size
+val_size = int(0.1 * len(dataset))
 train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
 
 # Data loaders for batching, shuffling, and parallel loading
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Neural network model: Convolutional Neural Network
 class SimpleCNN(nn.Module):
@@ -57,60 +65,103 @@ class SimpleCNN(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        # Other convolutional and fully connected layers follow similarly...
-
-        # Final fully connected layer for binary classification
+        # Second Convolutional Layer
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
+        # Third Convolutional Layer
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
+        # Fourth Convolutional Layer
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.pool4 = nn.MaxPool2d(kernel_size=3, stride=2)
+        
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(128*4, 128)
+        # self.fc1 = nn.Linear(32*512, 128)
+        self.dropout = nn.Dropout(0.3)
         self.fc2 = nn.Linear(128, 2)
-
+        
     def forward(self, x):
-        # Forward pass for the first convolutional layer
         x = self.conv1(x)
         x = self.bn1(x)
         x = F.relu(x)
         x = self.pool1(x)
         
-        # Forward passes for other layers...
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
         
-        # Softmax activation for binary classification
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+        
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = F.relu(x)
+        x = self.pool4(x)
+        
+        # Flatten the output
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = F.softmax(self.fc2(x), dim=1)
         
         return x
 
-# Initialize model, loss function, and optimizer
+
+# Initialize model, loss function, and optimizer   
 model = SimpleCNN()
+print(model)
+
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # Training and validation loops
-for epoch in range(100):
+for epoch in range(1):
+    count = 0
     model.train()
-    for inputs, labels in train_loader:
+    for inputs, labels in train_loader: # Assuming train_loader is your dataloader
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
+        count += 1
+        if count % 1000 == 0:
+            print(f"Epoch {epoch+1},  Count {count+1}, Loss: {loss.item()}")        
 
-    # Validation accuracy calculation
-    model.eval()
+
+     # Validation loop
+    model.eval()  # Set the model to evaluation mode
     correct = 0
     total = 0
-    with torch.no_grad():
-        for inputs, labels in val_loader:
+    with torch.no_grad():  # No need to track gradients when evaluating
+        for inputs, labels in val_loader:  # Assuming val_loader is your validation dataloader
             outputs = model(inputs)
             _, predicted = outputs.max(1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
+            
     accuracy = 100 * correct / total
     print(f"Epoch {epoch+1}, Training Loss: {loss.item()}, Validation Accuracy: {accuracy}%")
+
+# Save model
+torch.save(model, 'cnn_model.pth')
 
 # Evaluation on test set
 model.eval()
 correct = 0
 total = 0
 with torch.no_grad():
-    for inputs, labels in test_loader:
+    for inputs, labels in test_loader: # Assuming test_loader is your dataloader
         outputs = model(inputs)
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
@@ -118,7 +169,8 @@ with torch.no_grad():
 
 print(f"Accuracy: {100 * correct / total}%")
 
-# Confusion matrix visualization
+
+# Plot confusion matrix
 y_true = []
 y_pred = []
 with torch.no_grad():
